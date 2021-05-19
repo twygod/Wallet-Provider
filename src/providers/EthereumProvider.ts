b51a164ff413d7e057c3742a8497e788abf24ee7
twygod <twygod@gmail.com>
2021-05-19 19:31:20 +0800

    chg: change ethereumProvider.

--- src/providers/EthereumProvider.ts
+++ src/providers/EthereumProvider.ts
@@ -1,28 +1,26 @@
 /**
- * WalletProvider 
- * @license MIT 
+ * WalletProvider
+ * @license MIT
  * @author https://github.com/libertypie
  */
-
+import { providers } from "ethers";
 import Web3Standard from "./Web3Standard";
-import Provider from '../interface/Provider';
-import Exception from '../classes/Exception';
+import Provider from "../interface/Provider";
+import Exception from "../classes/Exception";
 
 const win = window as any;
-class EthereumProvider  extends Web3Standard implements Provider {
-
-    async _initialize(providerInfo: any){
-        
-        let provider = win.ethereum || win.web3;
-        console.log(provider)
-        if(!provider){
-            this._onConnectCallback(
-                new Exception("web3_wallet_not_found","Web3 Wallet was not found")
-            )
-        }
-        this.setProvider(provider)
+class EthereumProvider extends Web3Standard implements Provider {
+  async _initialize(providerInfo: any) {
+    let provider = new providers.Web3Provider(
+      win.ethereum || win.web3.currentProvider
+    );
+    if (!provider) {
+      this._onConnectCallback(
+        new Exception("web3_wallet_not_found", "Web3 Wallet was not found")
+      );
     }
-
-}    
+    this.setProvider(provider);
+  }
+}
 
 export default EthereumProvider;
